7a0f28c1e97a06a89c007221f2e19492f93e5a92
Aciss <wawa22arpk@gmail.com>
2026-08-06 18:54:43 +0700

    Inherit Dolby Atmos configurations in device.mk

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Inherit from Dolby Atmos
+$(call inherit-product, vendor/sony/dolby/sonydolby.mk)
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
